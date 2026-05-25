633809668501753c9a8c1cb2dd8d804db72df0a9
Nitin9967 <nitingupta10a@gmail.com>
2026-05-25 20:36:40 +0530

    Updated Model Version

--- services.py
+++ services.py
@@ -264,7 +264,7 @@ Return EXACT schema:
 """
 
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
+        model="gemini-3.5-flash",
         contents=prompt
     )
 
